894434473e1abc81ea882eeb22ddc9472952e040
lika66 <lilia_pandova@abv.bg>
2017-06-04 09:34:28 +0300

    First Commit

---
+++ js.js
@@ -0,0 +1,34 @@
+   var form = document.getElementById("myForm");
+   form.addEventListener("submit", myFunction);
+
+    function myFunction(e) {
+        e.preventDefault();
+        var number = document.getElementById('input-number').value;
+        var  text = document.getElementById('input-text');
+
+        var ones = ['', ' one', ' two', ' three', ' four', ' five', ' six', ' seven', ' eight', ' nine', ' ten', ' eleven', ' twelve', ' thirteen', ' fourteen', ' fifteen', ' sixteen', ' seventeen', ' eighteen', ' nineteen'];
+        var tens = ['', '', ' twenty', ' thirty', ' forty', ' fifty', ' sixty', ' seventy', ' eighty', ' ninety'];
+        var hundred = ' hundred';
+        var output = '';
+        var numString = number.toString();
+
+        if (number == 0){
+
+            output = 'zero';
+            text.value = output;
+        }
+        else if (number < 20){
+            output = ones[number];
+            text.value = output;
+        }
+        else if (number > 20 && number < 100){
+            output = (tens[Math.floor(number/10)] + " " + ones[number % 10]);
+            text.value = output;
+        }
+        else {
+            var devidedTens = number % 100;
+            output = (ones[Math.floor(number/100)] + " " + hundred + " " + (tens[Math.floor(devidedTens / 10)]) + " " + ones[number % 10]  );
+            text.value = output;
+        }
+
+    }
